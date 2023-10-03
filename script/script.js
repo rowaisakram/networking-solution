b5b91112b62a779ebca1beb0682502b8d0b6dc9b
Owais <r.owaisakram@gmail.com>
2023-10-03 17:30:04 +0500

    banner-4

--- script/script.js
+++ script/script.js
@@ -16,3 +16,24 @@ $(document).ready(function () {
       );
   });
 });
+
+var swiper = new Swiper(".content", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  grabCursor: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+});
